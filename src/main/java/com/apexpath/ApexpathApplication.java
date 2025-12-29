e3a32f258742ef62bc41029e52f4c1ff27a69029
Pedro Novais <phenrique101007@gmail.com>
2025-12-29 04:29:18 -0300

    Fisrt commit

---
+++ src/main/java/com/apexpath/ApexpathApplication.java
@@ -0,0 +1,13 @@
+package com.apexpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexpathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexpathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apexpath/ApexpathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexpathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
